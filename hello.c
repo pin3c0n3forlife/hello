a40706e6071a7505b26e75cff7b21c5ddcacf0a8
Angel Padilla Diaz Jr <apadilladiazjr2@2@uskers.unl.edu>
2022-08-25 19:26:32 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ *Author: Angel Padilla Diaz Jr.
+ *Date: 202208/25
+ *
+ * A simple hello program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Angel Padilla Diaz Jr.\n");
+
+  return 0;
+}
